94551e83547637126726ad4b1a670d3d015e64d4
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-05-01 23:55:06 +0530

    login and signup acitvity update and changes

--- MyApplication/app/src/main/java/com/example/myapplication/loginTab.java
+++ MyApplication/app/src/main/java/com/example/myapplication/loginTab.java
@@ -1,24 +1,27 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class loginTab extends AppCompatActivity {
-
+Button signup_btn1st;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_tab);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        signup_btn1st = findViewById(R.id.signup_btn1st);
+        signup_btn1st.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(loginTab.this,signupTab.class);
+                startActivity(intent);
+            }
         });
     }
 }
--- MyApplication/app/src/main/java/com/example/myapplication/signupTab.java
+++ MyApplication/app/src/main/java/com/example/myapplication/signupTab.java
@@ -1,24 +1,27 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class signupTab extends AppCompatActivity {
-
+Button login_btn1st;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signup_tab);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        login_btn1st = findViewById(R.id.login_btn1st);
+        login_btn1st.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(signupTab.this,loginTab.class);
+                startActivity(intent);
+            }
         });
     }
 }
