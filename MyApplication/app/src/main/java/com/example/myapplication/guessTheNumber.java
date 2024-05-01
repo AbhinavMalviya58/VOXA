74600d3f80cd63cb1997f1f4ce40455dc3273b89
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-05-01 23:20:27 +0530

    add logic

--- MyApplication/app/src/main/java/com/example/myapplication/guessTheNumber.java
+++ MyApplication/app/src/main/java/com/example/myapplication/guessTheNumber.java
@@ -1,25 +1,35 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class guessTheNumber extends AppCompatActivity {
 
-    Button button;
+    EditText guessTheNumber;
+    TextView guessTheNumberTextView;
+    Button guessTheNumberButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_guess_the_number);
 
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
+//        guessTheNumber.findViewById(R.id.guessTheNumber);
+//        guessTheNumberTextView.findViewById(R.id.guessTheNumberTextView);
+        guessTheNumberButton.findViewById(R.id.guessTheNumberBtn);
+
+        guessTheNumberButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(guessTheNumber.this, " xyz ", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
