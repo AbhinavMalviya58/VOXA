3beb74b44b79df616f4e91a186d6f8452d7b270b
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-05-05 19:45:44 +0530

    gridle update 05May24

--- MyApplication/app/build.gradle.kts
+++ MyApplication/app/build.gradle.kts
@@ -60,6 +60,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.airbnb.android:lottie:6.4.0")
-
-
+    implementation("com.github.AtifSayings:Animatoo:1.0.1")
 }
