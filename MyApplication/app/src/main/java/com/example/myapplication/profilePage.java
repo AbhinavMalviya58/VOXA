26c3197d52f34fddd5a2afe0e371bf5d0e4b5ba1
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-05-05 20:34:13 +0530

    modifide profile page

--- MyApplication/app/src/main/java/com/example/myapplication/profilePage.java
+++ MyApplication/app/src/main/java/com/example/myapplication/profilePage.java
@@ -1,25 +1,70 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.Objects;
 
 public class profilePage extends AppCompatActivity {
-    TextView shoEmail,shoName;
-
+    TextView shoEmail,shoName,user;
+    ImageView userImg;
+    Button logout_button;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    GoogleSignInClient gsc;
+    GoogleSignInOptions gso;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
 
+        shoName = findViewById(R.id.shoName);
+        shoEmail = findViewById(R.id.shoEmail);
+        user = findViewById(R.id.user);
+        logout_button = findViewById(R.id.logout_button);
+        userImg = findViewById(R.id.userImg);
 
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if(account != null){
+            String userName = account.getDisplayName();
+            String userEmail = account.getEmail();
+            user.setText(userName);
+            shoName.setText(userName);
+            shoEmail.setText(userEmail);
+//            Uri Img = account.getPhotoUrl();
+//            userImg.setImageBitmap(Img);
+        }
 
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this,gso);
+
+        logout_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
 //        Toolbar
         Toolbar toolbar = findViewById(R.id.materialToolbar);
         setSupportActionBar(toolbar);
@@ -32,5 +77,15 @@ public class profilePage extends AppCompatActivity {
             finish();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    void logout(){
+        gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                finish();
+                startActivity(new Intent(profilePage.this, loginTab.class));
+            }
+        });
     }
 }
