fd5541d4299cd220cf967f98da1d24a72ca3d819
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-05-10 23:03:36 +0530

    app update

--- MyApplication/app/build.gradle.kts
+++ MyApplication/app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 }
 
 dependencies {
+    implementation ("com.github.iammert:ReadableBottomBar:0.2")
     // Add the dependency for the Firebase Authentication library
     // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-auth")
@@ -61,6 +62,5 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.airbnb.android:lottie:6.4.0")
     implementation("com.github.AtifSayings:Animatoo:1.0.1")
-    implementation ("com.github.iammert:ReadableBottomBar:0.2")
 
 }
