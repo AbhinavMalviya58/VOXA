104febc19645dad26fe25a2532815979a1bfff0b
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-05-14 16:59:59 +0530

    gradle.kts

--- MyApplication/app/build.gradle.kts
+++ MyApplication/app/build.gradle.kts
@@ -37,7 +37,6 @@ android {
 }
 
 dependencies {
-    implementation ("com.github.iammert:ReadableBottomBar:0.2")
     // Add the dependency for the Firebase Authentication library
     // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-auth")
