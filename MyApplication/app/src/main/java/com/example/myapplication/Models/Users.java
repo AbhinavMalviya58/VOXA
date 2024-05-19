c53a0cd2cd27e86d02352e205c726f7957844021
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-05-19 10:17:20 +0530

    add models for user

--- MyApplication/app/src/main/java/com/example/myapplication/Models/Users.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Models/Users.java
@@ -1,2 +1,68 @@
-package com.example.myapplication.Models;public class Users {
+package com.example.myapplication.Models;
+
+public class Users {
+    String profilePic , userMame , mail , password , userId , lastMessage;
+
+    public Users(String profilePic, String userMame, String mail, String password, String userId, String lastMessage) {
+        this.profilePic = profilePic;
+        this.userMame = userMame;
+        this.mail = mail;
+        this.password = password;
+        this.userId = userId;
+        this.lastMessage = lastMessage;
+    }
+
+    public Users(){
+
+    }
+
+    // SignUp
+
+    public String getProfilePic() {
+        return profilePic;
+    }
+
+    public void setProfilePic(String profilePic) {
+        this.profilePic = profilePic;
+    }
+
+    public String getUserMame() {
+        return userMame;
+    }
+
+    public void setUserMame(String userMame) {
+        this.userMame = userMame;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getLastMessage() {
+        return lastMessage;
+    }
+
+    public void setLastMessage(String lastMessage) {
+        this.lastMessage = lastMessage;
+    }
 }
