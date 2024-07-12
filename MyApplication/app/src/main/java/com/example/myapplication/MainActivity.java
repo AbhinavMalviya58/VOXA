aa1d669948caa32c0c1a586a9462349bda20efe3
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-07-12 23:15:23 +0530

    update

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,17 +15,26 @@ import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 
 public class MainActivity extends AppCompatActivity {
     ImageView profilePic;
+    TextView guessTheNumber;
     Animation animation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         profilePic = findViewById(R.id.profilePic);
+        guessTheNumber = findViewById(R.id.guessTheNumber);
 
         animation = AnimationUtils.loadAnimation(MainActivity.this,R.anim.imganim);
         profilePic.startAnimation(animation);
+
+        guessTheNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Guess The Number", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, guessTheNumber.class));
+            }
+        });
 
         profilePic.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ MyApplication/app/src/main/java/com/example/myapplication/guessTheNumber.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class guessTheNumber extends AppCompatActivity {
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_guess_the_number);
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+
+    }
+}
