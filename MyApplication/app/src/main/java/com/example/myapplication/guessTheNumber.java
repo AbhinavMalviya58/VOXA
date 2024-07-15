2a24698e219b581e94479668f5b8bf34fffa3050
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-07-15 20:14:35 +0530

    no update

--- MyApplication/app/src/main/java/com/example/myapplication/guessTheNumber.java
+++ MyApplication/app/src/main/java/com/example/myapplication/guessTheNumber.java
@@ -1,12 +1,11 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class guessTheNumber extends AppCompatActivity {
@@ -14,22 +13,23 @@ public class guessTheNumber extends AppCompatActivity {
     EditText guessTheNumber;
     TextView guessTheNumberTextView;
     Button guessTheNumberButton;
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_guess_the_number);
 
-//        guessTheNumber.findViewById(R.id.guessTheNumber);
-//        guessTheNumberTextView.findViewById(R.id.guessTheNumberTextView);
-        guessTheNumberButton.findViewById(R.id.guessTheNumberBtn);
-
-        guessTheNumberButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(guessTheNumber.this, " xyz ", Toast.LENGTH_SHORT).show();
-            }
-        });
+        guessTheNumber.findViewById(R.id.guessTheNumber);
+        guessTheNumberTextView.findViewById(R.id.guessTheNumberTextView);
+//        guessTheNumberButton.findViewById(R.id.guessTheNumberBtn);
+//
+//        guessTheNumberButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(guessTheNumber.this, " xyz ", Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
     }
 }
