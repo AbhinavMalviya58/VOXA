df3f4ed06a23e3d37c1794d143a5e2ea09ccfcd3
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-07-18 15:18:40 +0530

    guess the number Activity bug fix

--- MyApplication/app/src/main/java/com/example/myapplication/guessTheNumber.java
+++ MyApplication/app/src/main/java/com/example/myapplication/guessTheNumber.java
@@ -1,11 +1,12 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class guessTheNumber extends AppCompatActivity {
@@ -17,19 +18,30 @@ public class guessTheNumber extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_guess_the_number);
 
-        guessTheNumber.findViewById(R.id.guessTheNumber);
-        guessTheNumberTextView.findViewById(R.id.guessTheNumberTextView);
-//        guessTheNumberButton.findViewById(R.id.guessTheNumberBtn);
-//
-//        guessTheNumberButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Toast.makeText(guessTheNumber.this, " xyz ", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+        guessTheNumber = findViewById(R.id.guessTheNumber);
+        guessTheNumberTextView = findViewById(R.id.guessTheNumberTextView);
+        guessTheNumberButton = findViewById(R.id.guessTheNumberBtn);
+
+//        Random random = new Random();
+//        int numberToGuess = random.nextInt(100) + 1; // Generates a random number between 1 and 100
+//        int numberOfTries = 0;
+//        boolean hasGuessedCorrectly = false;
+        guessTheNumberButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = guessTheNumber.getText().toString();
+                if(s.isEmpty()){
+                    guessTheNumber.setError("Empty");
+                    Toast.makeText(guessTheNumber.this, "Number is empty", Toast.LENGTH_SHORT).show();
+                }else {
+                int kg = Integer.parseInt(s);
+                double pound = 2.205 * kg;
+                guessTheNumberTextView.setText("The corresponding value in pound is " + pound);
+                }
+            }
+        });
 
     }
 }
