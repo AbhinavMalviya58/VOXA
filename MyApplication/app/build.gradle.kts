e09cde7ab07aaa045dee3d2877d1be6ca372a482
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2024-08-01 14:54:30 +0530

    build.gradle.kts

--- MyApplication/app/build.gradle.kts
+++ MyApplication/app/build.gradle.kts
@@ -61,5 +61,7 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.airbnb.android:lottie:6.4.0")
     implementation("com.github.AtifSayings:Animatoo:1.0.1")
+    implementation ("pl.droidsonroids.gif:android-gif-drawable:1.2.29")
+
 
 }
